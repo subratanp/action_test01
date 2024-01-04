82517d1f34423918ce317dbb0a4a5f660f560509
Subrata <bisw1sub@police.nsw.gov.au>
2024-01-04 12:16:20 +1100

    Testing Now

--- actiontest.js
+++ actiontest.js
@@ -6,7 +6,7 @@ let github=require("@actions/github");
 console.log(github.context.repo);
 let reponame= github.context.repo.repo
 let crypto=require("crypto");
-let json=require("../../"+ reponame+"folderlist.json");
+let json=require("../../"+ reponame+"/folderlist.json");
 console.log(json.artifacts);
 var provenance={
     "_type": "https://in-toto.io/Statement/v1",
@@ -60,5 +60,7 @@ for (i of json.artifacts){
 console.log(JSON.stringify(provenance));
 
 var provenance_write=file.writeFileSync("provenance.json",JSON.stringify(provenance));
+
+
 
 
